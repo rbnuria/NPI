4b610e1815d99dc358c9151329d59962efa2854a
rbnuria <rbnuria6@gmail.com>
2018-01-02 20:06:33 +0100

    Quitando cosillas game_activity

--- MuseoCajaGranada/app/src/main/java/com/example/jlsuarezdiaz/museocajagranada/GameStartActivity.java
+++ MuseoCajaGranada/app/src/main/java/com/example/jlsuarezdiaz/museocajagranada/GameStartActivity.java
@@ -2,9 +2,7 @@ package com.example.jlsuarezdiaz.museocajagranada;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -27,14 +25,14 @@ public class GameStartActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
